c952f6ada9528d8332d8e040a4e1b1cefeb7b1c0
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-05-14 17:35:14 -0700

    add edge cases

--- Strings/isPalindrome.js
+++ Strings/isPalindrome.js
@@ -1,11 +1,11 @@
 const isPalindrome = function (s) {
-  s = s.replace(/\s/g, "").toLowerCase();
-
+  s = s.replace(/\s/g, "", ).replace(/,/g, '').replace(/:/g, '').toLowerCase();
+  console.log(s)
   let stringReverse = s.split("").reverse().join("");
-
+  console.log(stringReverse)
   return s == stringReverse;
 };
 
-console.log(isPalindrome("A man a plan a canal Panama"));
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
 console.log(isPalindrome("race a car"));
 console.log(isPalindrome("toot"));
